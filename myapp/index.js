b3a6529295c2a26a5a211a490b4b1be0e15924e3
Ryan-Phillips Cornelio <ryan.cornelio@gmail.com>
2021-09-06 15:56:43 +0200

    Cut out temporary files. Added task array.

--- myapp/index.js
+++ myapp/index.js
@@ -1,27 +1,83 @@
+const path = require('path');
 const express = require('express');
+const { json } = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.get('/api', (req, res) => {
-    res.send('Hello World!')
-});
 
-app.use('/static', express.static('public'))
+function generateId() {
+    return Math.floor(Math.random() * 100).toString();
+};
+
+let tasks = [
+    {
+        id: generateId(),
+        name: "Clean"
+    },
+    {
+        id: generateId(),
+        name: "Cook food"
+    },
+    {
+        id: generateId(),
+        name: "Walk the dog"
+    },
+    {
+        id: generateId(),
+        name: "Code"
+    },
+    {
+        id: generateId(),
+        name: "Clean window"
+    }
+    
+];
+// Client folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Server
+app.listen(port, () => console.log(`App is on port: ${port}!`));
+
+
+// Body parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+try {
+    app.get('/tasks', function (req, res) {
+        res.json(artists)
+    });
+} catch (err) {
+    console.log('error')
+}
 
 app.use(express.static('public'))
 
-app.listen(port, () => {
-    console.log("Applikationen är igång, välkommen!")
+app.post('/task', (req, res) => {
+    // We will be coding here
+    try {
+        const task = {
+            id: generateId(),
+            name: req.body.name
+        };
+        tasks.push(task);
+        res.json('added')
+    } catch {
+        console.error();
+    }
+
+    // Output the task to the console for debugging
+    console.log(task);
+    tasks.push(task);
+
+    res.send('Task is added to the database');
 });
 
-app.post('/', function (req, res) {
-    res.send('Got a POST request')
-});
-
-app.put('/', function (req, res) {
+/* app.put('/', function (req, res) {
     res.send('Got a PUT request at /user')
 });
 
 app.delete('/', function (req, res) {
     res.send('Got a DELETE request at /user')
-})
+}) */
--- myapp/public/logic.js
+++ myapp/public/logic.js
@@ -1,3 +1,15 @@
-function loadText() {
-    console.log('Secret text...')
+
+function send() {
+    console.log('Welcome to Inter Solutions')
+};
+
+function getTask() {
+    var str = document.getElementById("text1").value;
+    console.log("Task: " + str)
+}
+
+
+function blast() {
+    var all = document.getElementById("submit").value;
+    getTask();
 }
